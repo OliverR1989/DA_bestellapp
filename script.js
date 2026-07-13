30dfa44871dfb871a3624ad84e0e273ea84e6407
Oliver Rittinghaus <rittinghausoliver@gmail.com>
2026-07-13 17:16:44 +0200

    modal functions added

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-function openModal(modal){
+const modal = document.getElementById('modal');
+console.log(modal);
+
+function openModal(){
     document.getElementById('modal').classList.add('active');
+    document.getElementById('basket').classList.add('hidden');
     document.body.style.overflow = 'hidden';
 }
 
-function closeModal(modal){
+function closeModal(){
     document.getElementById('modal').classList.remove('active');
     document.body.style.overflow = '';
 }
+
+document.addEventListener('click', function(e) {
+    const activeModal = document.querySelector('.delivery-modal.active');
+    if (e.target === modal) {
+        activeModal.classList.remove('active');
+        document.body.style.overflow = '';
+    }
+})
+
+document.addEventListener('keydown', function(e){
+    if (e.key === 'Escape') {
+        const activeModal = document.querySelector('.delivery-modal.active');
+        if (activeModal) {
+            activeModal.classList.remove('active');
+            document.body.style.overflow = '';
+        }
+    }
+})
+
+setTimeout(closeModal, 2500);
