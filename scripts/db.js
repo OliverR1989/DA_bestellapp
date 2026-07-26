3684380883b68f0bca14738d1a842e65db89132f
Oliver Rittinghaus <rittinghausoliver@gmail.com>
2026-07-26 12:20:48 +0200

    db array changed

--- scripts/db.js
+++ scripts/db.js
@@ -2,98 +2,86 @@ const deliverCost = 4.90
 const menuData = [
   {
     category: "Maki",
-    items: [
-      {
-        name: "Avocado Maki",
-        description: "Klassische Reisrolle mit frischer Avocado",
-        price: 5.90,
-        image: "./assets/img/menu/avocado-maki.png",
-      },
-      {
-        name: "Lachs Maki",
-        description: "Reisrolle mit frischem Lachs und Nori",
-        price: 6.90,
-        image: "./assets/img/menu/lachs-maki.png",
-      },
-      {
-        name: "Gurken Maki",
-        description: "Vegetarische Rolle mit knackiger Gurke",
-        price: 4.90,
-        image: "./assets/img/menu/gurken-maki.png",
-      },
-    ]
+    name: "Avocado Maki",
+    description: "Klassische Reisrolle mit frischer Avocado",
+    price: 5.90,
+    image: "./assets/img/menu/avocado-maki.png",
+  },
+  {
+    category: "Maki",
+    name: "Lachs Maki",
+    description: "Reisrolle mit frischem Lachs und Nori",
+    price: 6.90,
+    image: "./assets/img/menu/lachs-maki.png",
+  },
+  {
+    category: "Maki",
+    name: "Gurken Maki",
+    description: "Vegetarische Rolle mit knackiger Gurke",
+    price: 4.90,
+    image: "./assets/img/menu/gurken-maki.png",
   },
   {
     category: "Nigiri",
-    items: [
-      {
-        name: "Lachs Nigiri",
-        description: "Handgeformter Reis mit frischer Lachsscheibe",
-        price: 3.50,
-        image: "./assets/img/menu/lachs-nigiri.png",
-      },
-      {
-        name: "Thunfisch Nigiri",
-        description: "Handgeformter Reis mit magerem Thunfisch",
-        price: 3.90,
-        image: "./assets/img/menu/thunfisch-nigiri.png",
-      },
-      {
-        name: "Garnelen Nigiri",
-        description: "Handgeformter Reis mit gekochter Garnele",
-        price: 3.20,
-        image: "./assets/img/menu/garnelen-nigiri.png",
-      }
-    ]
+    name: "Lachs Nigiri",
+    description: "Handgeformter Reis mit frischer Lachsscheibe",
+    price: 3.50,
+    image: "./assets/img/menu/lachs-nigiri.png",
+  },
+  {
+    category: "Nigiri",
+    name: "Thunfisch Nigiri",
+    description: "Handgeformter Reis mit magerem Thunfisch",
+    price: 3.90,
+    image: "./assets/img/menu/thunfisch-nigiri.png",
+  },
+  {
+    category: "Nigiri",
+    name: "Garnelen Nigiri",
+    description: "Handgeformter Reis mit gekochter Garnele",
+    price: 3.20,
+    image: "./assets/img/menu/garnelen-nigiri.png",
   },
   {
     category: "Dessert",
-    items: [
-      {
-        name: "Mochi Erdbeere",
-        description: "Weiches Reiskuchen-Dessert mit Erdbeerfüllung",
-        price: 4.50,
-        image: "./assets/img/menu/mochi-dessert.png",
-      },
-      {
-        name: "Mango Sticky Rice",
-        description: "Klebreis mit süßer Kokosmilch und frischer Mango",
-        price: 5.50,
-        image: "./assets/img/menu/mango-dessert.png",
-      },
-      {
-        name: "Green Tea Eis",
-        description: "Cremiges Matcha-Eis, leicht und erfrischend",
-        price: 3.90,
-        image: "./assets/img/menu/ice-dessert.png",
-      }
-    ]
+    name: "Mochi Erdbeere",
+    description: "Weiches Reiskuchen-Dessert mit Erdbeerfüllung",
+    price: 4.50,
+    image: "./assets/img/menu/mochi-dessert.png",
+  },
+  {
+    category: "Dessert",
+    name: "Mango Sticky Rice",
+    description: "Klebreis mit süßer Kokosmilch und frischer Mango",
+    price: 5.50,
+    image: "./assets/img/menu/mango-dessert.png",
+  },
+  {
+    category: "Dessert",
+    name: "Green Tea Eis",
+    description: "Cremiges Matcha-Eis, leicht und erfrischend",
+    price: 3.90,
+    image: "./assets/img/menu/ice-dessert.png",
   },
   {
     category: "Getraenke",
-    items: [
-      {
-        name: "Grüner Tee",
-        description: "Traditioneller japanischer Grüntee, warm serviert",
-        price: 2.50,
-        image: "./assets/img/menu/tea-drinks.png",
-      },
-      {
-        name: "Ramune",
-        description: "Japanische Limonade mit fruchtigem Geschmack",
-        price: 3.20,
-        image: "./assets/img/menu/ramune-drinks.png",
-      },
-      {
-        name: "Mineralwasser",
-        description: "Still oder mit Kohlensäure",
-        price: 2.20,
-        image: "./assets/img/menu/water-drinks.png",
-      }
-    ]
-  }
-];
-
-
-
-
+    name: "Grüner Tee",
+    description: "Traditioneller japanischer Grüntee, warm serviert",
+    price: 2.50,
+    image: "./assets/img/menu/tea-drinks.png",
+  },
+  {
+    category: "Getraenke",
+    name: "Ramune",
+    description: "Japanische Limonade mit fruchtigem Geschmack",
+    price: 3.20,
+    image: "./assets/img/menu/ramune-drinks.png",
+  },
+  {
+    category: "Getraenke",
+    name: "Mineralwasser",
+    description: "Still oder mit Kohlensäure",
+    price: 2.20,
+    image: "./assets/img/menu/water-drinks.png",
+  },
+]
